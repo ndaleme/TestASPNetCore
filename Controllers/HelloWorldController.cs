b2c6764f63af92bc019574dec7d85cf7302447ab
nde <36724834+nde@users.noreply.github.com>
2018-02-22 15:33:37 +0100

    Initial commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestASPNetCore.Controllers
+{
+    public class HelloWorldController
+    {
+
+        private static Dictionary<string, string> _dico = new Dictionary<string, string>();
+
+        [HttpGet("display/{val}")]
+        public string HelloWorld(string val)
+        {
+            return val;
+        }
+
+        //[HttpGet("dico/add/{key}")]
+        //public string Add(string key, string val)
+        //{
+        //    _dico[key] = val;
+
+        //    return String.Format("{0} added to the dico!", key);
+        //}
+
+        [HttpPost("dico/{key}")]
+        public string Post(string key, [FromBody] string val)
+        {
+            _dico[key] = val;
+
+            return String.Format("{0} added to the dico!", key);
+        }
+
+        [HttpGet("dico/{key}")]
+        public string Get(string key)
+        {
+            if (!_dico.ContainsKey(key))
+            {
+                return String.Format(" key {0} not found in the dico!", key);
+            }
+            return _dico[key];
+        }
+
+
+        [HttpPatch("dico/{key}")]
+        public string Update(string key, [FromBody] string val)
+        {
+            if (!_dico.ContainsKey(key))
+            {
+                return String.Format(" key {0} not found in the dico!", key);
+            }
+
+            _dico[key] = val;
+
+            return "dico updated.";
+        }
+
+        [HttpDelete("dico/{key}")]
+        public string Delete(string key)
+        {
+            if (!_dico.ContainsKey(key))
+            {
+                return String.Format(" key {0} not found in the dico!", key);
+            }
+
+            _dico.Remove(key);
+
+            return "key deleted.";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration; // 1st line added
+
+namespace TestASPNetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var config = new ConfigurationBuilder().AddEnvironmentVariables("").Build();
+            // 2nd line added
+
+            var url = config["ASPNETCORE_URLS"] ?? "http://*:8080";
+            // 3rd line added
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
